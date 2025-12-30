5ffc1902aa1e8462dea9346443c4a79d5feedaa9
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2025-12-30 10:33:52 +0100

    Tried to make BG colors of bubbles even darker for better contrast

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ const plantCategories = [
 
 function randomHsl() {
   const hue = Math.floor(Math.random() * 360);
-  const saturation = 70;
-  const lightness = Math.floor(Math.random() * 30) + 20;
+  const saturation = Math.floor(Math.random() * 30) + 60;
+  const lightness = Math.floor(Math.random() * 20) + 15;
 
   return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 }
